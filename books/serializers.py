4dc6aa89911dd1dc7709922cfb6446fd3eebff4e
Afrizaaa017 <afrizaharapan@gmail.com>
2025-08-26 12:05:28 +0700

    feat: Implement validation for book model

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,27 @@
 from rest_framework import serializers
 from .models import Book
+from rest_framework.validators import UniqueValidator
 
 class BookSerializer(serializers.ModelSerializer):
+    # Django / DRF validation 
+    title = serializers.CharField(
+        validators=[
+            UniqueValidator(queryset=Book.objects.all(), message="Judul sudah ada.")
+        ] 
+    )
+
     class Meta:
         model = Book
         fields = '__all__'
+
+    # Field-level validation
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Title must be at least 3 characters long.")
+        return value
+
+    # Object-level validation (Kombinasi beberapa field)
+    def validate(self, data):
+        if data['title'].lower() == data['author'].lower():
+            raise serializers.ValidationError("Judul dan penulis tidak boleh sama.")
+        return data
